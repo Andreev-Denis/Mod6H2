6665741f45bfea619a8feda18ee248e7027d4678
Andreev-Denis <163521561+Andreev-Denis@users.noreply.github.com>
2024-05-13 12:36:47 +0300

    Add files via upload

---
+++ probe.py
@@ -0,0 +1,27 @@
+class Vehicle():
+    vehicle_type = None
+    def __str__(self):
+        return 'Vehicle type ={}'.format(self.vehicle_type)
+
+class Car():
+    price = 1000000
+
+    def horse_powers(self):
+        return 100
+
+class Nissan (Car, Vehicle):
+    price = 150000
+
+    vehicle_type = 'Sportcar'
+
+    def horse_powers(self):
+        return 240
+
+
+My_Car = Nissan()
+My_Car.price
+My_Car.vehicle_type
+My_Car.horse_powers
+print(My_Car.vehicle_type)
+print(My_Car.price)
+print(My_Car.horse_powers)
